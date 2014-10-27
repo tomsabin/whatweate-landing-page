da53f6cd35736d657305dcbb56fa242821d8f3fb
Tom Sabin <tom.sabin@unboxedconsulting.com>
2014-10-27 22:35:08 +0000

    Add MailChimp ajax implementation

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -5,4 +5,11 @@ $(document).ready(function () {
     }, 500);
     return false;
   });
+
+  $('#newsletter').ajaxChimp({
+    url: 'http://whatweate.us9.list-manage.com/subscribe/post?u=3ae9a01a53d464033afcc3009&id=eb7d06a555',
+    callback: function (data) {
+      $('#response').text(data.msg);
+    }
+  });
 });
