a192d5c6c036360a204b57c85e7b19ea3dd86a8a
Tom Sabin <tom.sabin@unboxedconsulting.com>
2014-11-02 19:11:15 +0000

    Add not found and error pages

--- app.rb
+++ app.rb
@@ -5,3 +5,11 @@ get '/' do
   @events = Meetup.events_with_photos
   erb :index, layout: :'layouts/application'
 end
+
+not_found do
+  erb :not_found, layout: :'layouts/application'
+end
+
+error do
+  erb :error, layout: :'layouts/application'
+end
