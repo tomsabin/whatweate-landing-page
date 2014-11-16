ebe79e2c8142e4d39417728eed87e5493a8da5e4
Tom Sabin <tom.sabin@unboxedconsulting.com>
2014-11-16 22:55:04 +0000

    Add Google Analytics event tracking to buttons and newsletter submission

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -9,7 +9,19 @@ $(document).ready(function () {
   $('#newsletter').ajaxChimp({
     url: 'http://whatweate.us9.list-manage.com/subscribe/post?u=3ae9a01a53d464033afcc3009&id=eb7d06a555',
     callback: function (data) {
+      ga('send', 'event', 'newsletter', 'submit', 'fill in newsletter');
       $('#response').text(data.msg);
+    }
+  });
+
+  $('[data-ga]').on('click', function () {
+    switch(this.getAttribute('data-ga')) {
+      case 'conversion':
+        ga('send', 'event', 'conversion', 'click', $(this).text().trim());
+      case 'events':
+        ga('send', 'event', 'conversion', 'click', this.getAttribute('title'));
+      case 'social':
+        ga('send', 'event', 'social', 'click', $(this).text().trim());
     }
   });
 });
